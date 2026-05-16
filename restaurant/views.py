1b7231ba188d4e2747f329adff12be16308bbf42
prerana-jena <159808146+prerana-jena@users.noreply.github.com>
2026-05-16 09:29:34 +0000

    Add files via upload

---
+++ restaurant/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Menu
+from .forms import BookingForm
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+def menu(request):
+    menu_data=Menu.objects.all()
+    main_data={'menu':menu_data}
+    return render(request, 'menu.html',main_data)
+# Create your views here.
+def display_menu_item(request,pk=None):
+    if pk:
+        menu_item = Menu.objects.get(id=pk)
+    else:
+        menu_item=""
+    return render(request,'menu_item.html',{'menu_item':menu_item})
